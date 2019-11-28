b0d5ac358f5119d7367ed119c6b254d58c545bf5
Lu Wen Qing <david1113531523@gmail.com>
2019-11-28 11:45:56 +0800

    项目初始化

---
+++ src/main/java/cn/ecnuer996/manager/PatientDataManagerApplication.java
@@ -0,0 +1,13 @@
+package cn.ecnuer996.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientDataManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientDataManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ecnuer996/manager/PatientDataManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ecnuer996.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientDataManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
