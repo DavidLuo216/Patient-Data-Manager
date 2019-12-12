17dde1e8036057eb0545cd6f770c7747ecbb4523
IHsing Chang <ihsingchang@CupNoodles.local>
2019-12-12 16:19:32 +0800

    Modified on PatientDao

--- src/main/java/cn/ecnuer996/manager/dao/PatientDao.java
+++ src/main/java/cn/ecnuer996/manager/dao/PatientDao.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -22,5 +23,25 @@ public class PatientDao {
         Query query=new Query(Criteria.where("_id").is(id));
         Patient patient=mongoTemplate.findOne(query,Patient.class);
         return patient;
+    }
+
+    //update
+    public void updatePatient(Patient patient){
+        Query query = new Query(Criteria.where("_id").is(patient.getId()));
+        Update update = new Update().set("_id",patient.getId())
+                .set("name",patient.getName())
+                .set("birthday",patient.getBirthday())
+                .set("gender",patient.getGender())
+                .set("phone",patient.getPhone())
+                .set("address",patient.getAddress())
+                .set("history",patient.getHistory())
+                .set("diagnose",patient.getDiagnose());
+        mongoTemplate.updateFirst(query,update,Patient.class);
+    }
+
+    //delete
+    public void deletePatient(String id){
+        Query query = new Query(Criteria.where("_id").is(id));
+        mongoTemplate.remove(query,Patient.class);
     }
 }
